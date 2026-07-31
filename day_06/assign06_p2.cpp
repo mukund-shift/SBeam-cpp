5b0e6894a12fae846519548a574529acc0d4c4f7
MUKUND <mukundsaroj.it@gmail.com>
2026-07-31 14:03:13 +0530

    +1

--- day_06/assign06_p2.cpp
+++ day_06/assign06_p2.cpp
@@ -20,28 +20,58 @@ class item{
 private:
     int id;
     string name;
-    static int price;
+    int price;
+    static int curr_price;
     static int curr_id;
 
 public:
     item(){
         this->id = item::curr_id;
-        this->name = "";
-        this->price = item::price;
+        this->name = "test";
+        this->price = item::curr_price;
         item::curr_id++;
-        item::price += 100;
+        item::curr_price += 100;
     }
     ~item(){
+    }
 
+    void set_id(int id){
+        this->id = id;        
+    }
+    void set_name(string name){
+        this->name = name;
+    }
+    void set_price(int price){
+        this->price = price;
+    }
+
+    int get_id(){
+        return this->id;
+    }
+    string get_name(){
+        return this->name;
+    }
+    int get_price(){
+        return this->price;
+    }
+
+    
+
+    static void display_all(item* array, int size){
+        for (int i = 0; i < size; i++){
+            cout << "Element no " << i + 1 << ": " << "ID-" << array[i].get_id() << " Name-" << array[i].get_name() << " Price-" << array[i].get_price() << endl;
+        }
     }
 };
 
 int item::curr_id = 1;
-int item::price = 100;
+int item::curr_price = 100;
 
 int main(){
     int n;
     cout << "Enter size:" << endl;
     cin >> n;
     item* array = new item[n];
+
+    item::display_all(array, n);
 }
