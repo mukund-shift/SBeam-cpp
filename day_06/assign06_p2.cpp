af43824bd0071da1870299a2d722a9b8d708821f
MUKUND <mukundsaroj.it@gmail.com>
2026-07-31 14:27:15 +0530

    day 6 done
    somewhat challenging

--- day_06/assign06_p2.cpp
+++ day_06/assign06_p2.cpp
@@ -15,7 +15,7 @@
 #include <string>
 using namespace std;
 
-// 3 wip, to do: write display_all class
+// 3 start
 class item{
 private:
     int id;
@@ -63,15 +63,74 @@ public:
         }
     }
 };
+// 3 done
+
+class ticket{
+    // lets make 2 static counters for half and full tickets each and then static func to calc total sales.
+    int price;
+    static int half_sold;
+    static int full_sold;
+
+public:
+    ticket(int price = 10){               // 5 for half and 10 for full
+        this->price = price;
+        if (price == 5)
+        {
+            ticket::half_sold++;
+        }
+        else if (price == 10)
+        {
+            ticket::full_sold++;
+        }
+    }
+    
+    static int total_sales(){
+        return (half_sold*5) + (full_sold*10);
+    }
+    static int half_sales(){
+        return (half_sold*5);
+    }
+    static int full_sales(){
+        return (full_sold*10);
+    }
+};
+
 
 int item::curr_id = 1;
 int item::curr_price = 100;
+int ticket::half_sold = 0;
+int ticket::full_sold = 0;
 
 int main(){
-    int n;
-    cout << "Enter size:" << endl;
-    cin >> n;
-    item* array = new item[n];
+    // 3
+    // int n;
+    // cout << "Enter size:" << endl;
+    // cin >> n;
+    // item* array = new item[n];
+    // item::display_all(array, n);
 
-    item::display_all(array, n);
+    // 4
+    // no arg means full ticket, 5 means half ticket
+    ticket t1(5);
+    ticket t2;
+    ticket t3;
+    ticket t4(5);
+    ticket t5;
+    ticket t6(5);
+    ticket t7(5);
+    ticket t8;
+    ticket t9(5);
+    ticket t10;
+    ticket t11(5);
+    ticket t12;
+    ticket t13;
+
+    int half_collection = ticket::half_sales();
+    cout << "Collection from half tickets: " << half_collection << endl;
+    int full_collection = ticket::full_sales();
+    cout << "Collection from full tickets: " << full_collection << endl;
+    int collection = ticket::total_sales();
+    cout << "Total collection: " << collection << endl;
+    return 0;
+
 }
